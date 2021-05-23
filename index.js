39fc8794538b08425494be0712e9b2e728c53169
Carl <carlbrainegebe@gmail.com>
2021-05-23 13:43:10 -0500

    commit tree d22a6ec1eade5ca587e6b14e9cf8b45bdf7b8217

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+//const { objectContaining } = require("expect");
+
 // Code your solution here
+function findMatching(arrayOfDrivers, name) {
+    const newArray = arrayOfDrivers.filter(  (arrayOfDrivers) => {  if(arrayOfDrivers.toLowerCase() === name.toLowerCase()) {
+        return name;
+    }
+    } );
+    return newArray;
+}
+
+function fuzzyMatch(arrayOfDrivers, name) {
+const newArray2 = arrayOfDrivers.filter(   (arrayOfDrivers) => {if(arrayOfDrivers[0] === name[0]) {
+return name;
+}}  );
+return newArray2;
+}
+
+
+function matchName(arrayOfDrivers, id) {
+
+   const newArray = arrayOfDrivers.filter(   function(ele) {return ele.name === id;}   )
+    return (newArray);
+
+//   console.log(arrayOfDrivers.filter(      (arrayOfDrivers) =>   {}     )
+//   )
+
+}
+
+
+// function matchName(arrayOfDrivers, name) {
+// const defined = fuzzyMatch(arrayOfDrivers, name);
+//     }
+    //bigArray.push(newArray);
